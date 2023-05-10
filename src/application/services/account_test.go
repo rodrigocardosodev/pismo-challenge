42e7bfbe4ce36eb927120586006cd180482df69f
Rodrigo Cardoso <rodrigocardoso@Rodrigos-MacBook-Air.local>
2023-05-10 11:43:20 -0300

    feat: create unit tests to account domain

---
+++ src/application/services/account_test.go
@@ -0,0 +1,41 @@
+package services_test
+
+import (
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	mock_models "github.com/rodrigocardosodev/pismo-challenge/src/application/mocks/models"
+	mock_ports "github.com/rodrigocardosodev/pismo-challenge/src/application/mocks/ports"
+	"github.com/rodrigocardosodev/pismo-challenge/src/application/services"
+	"github.com/stretchr/testify/require"
+)
+
+func TestAccountService_GetById(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	account := mock_models.NewMockAccountInterface(ctrl)
+	repository := mock_ports.NewMockIAccountRepository(ctrl)
+	repository.EXPECT().GetByID(gomock.Any()).Return(account, nil)
+
+	service := services.NewAccountService(repository)
+
+	result, err := service.GetByID(1)
+	require.Nil(t, err)
+	require.Equal(t, account, result)
+}
+
+func TestAccountService_Create(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	account := mock_models.NewMockAccountInterface(ctrl)
+	repository := mock_ports.NewMockIAccountRepository(ctrl)
+	repository.EXPECT().Create(gomock.Any()).Return(account, nil)
+
+	service := services.NewAccountService(repository)
+
+	result, err := service.Create("123456789")
+	require.Nil(t, err)
+	require.Equal(t, account, result)
+}
